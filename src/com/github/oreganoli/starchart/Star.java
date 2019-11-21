8b1a401c711c4e1c87f3fc97cc3f722c9221ac43
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-21 17:46:45 +0100

    Stars have names now

--- src/com/github/oreganoli/starchart/Star.java
+++ src/com/github/oreganoli/starchart/Star.java
@@ -5,4 +5,16 @@ public class Star {
     private static final double MAX_DECLINATION = +90.0;
     private String name;
     private String catalog_name;
+
+    public String name() {
+        return name;
+    }
+    public void set_name(String name) {
+        var new_name = name.toUpperCase();
+        if (new_name.matches("[A-Z]{3}[0-9]{4}")) {
+            this.name = new_name;
+        } else {
+            throw new IllegalArgumentException("Star names must be 3 Latin letters followed by 4 digits.");
+        }
+    }
 }
