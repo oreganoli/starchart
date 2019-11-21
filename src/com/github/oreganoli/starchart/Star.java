18e50a2a775470aa599b9b224e3302bb9ff24d16
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-21 18:24:18 +0100

    added a minimal temperature constant, renamed declination consts

--- src/com/github/oreganoli/starchart/Star.java
+++ src/com/github/oreganoli/starchart/Star.java
@@ -1,8 +1,9 @@
 package com.github.oreganoli.starchart;
 
 public class Star {
-    private static final double MIN_DECLINATION = -90.0;
-    private static final double MAX_DECLINATION = +90.0;
+    private static final double SOUTH_DECLINATION = -90.0;
+    private static final double NORTH_DECLINATION = +90.0;
+    private static final double MIN_TEMPERATURE = 2000.0;
     private String name;
     private String catalog_name;
 
