55f8a29549e27699c6107e5545c13a80b7d82dc2
oreganoli <3611916+oreganoli@users.noreply.github.com>
2019-11-21 17:36:39 +0100

    added the Greek alphabet

---
+++ src/com/github/oreganoli/starchart/Greek.java
@@ -0,0 +1,30 @@
+package com.github.oreganoli.starchart;
+
+public class Greek {
+    private static final String[] ALPHABET = {
+            "Alpha",
+            "Beta",
+            "Gamma",
+            "Delta",
+            "Epsilon",
+            "Zeta",
+            "Eta",
+            "Theta",
+            "Iota",
+            "Kappa",
+            "Lambda",
+            "Mu",
+            "Nu",
+            "Xi",
+            "Omicron",
+            "Pi",
+            "Rho",
+            "Sigma",
+            "Tau",
+            "Upsilon",
+            "Phi",
+            "Chi",
+            "Psi",
+            "Omega"
+    };
+}
