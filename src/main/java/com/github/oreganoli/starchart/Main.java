fc4d1f9e1e8379fbefad192503a747c636d38cd4
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-11 14:02:45 +0100

    switched back to the Gson JSON library

--- src/main/java/com/github/oreganoli/starchart/Main.java
+++ src/main/java/com/github/oreganoli/starchart/Main.java
@@ -1,7 +1,10 @@
 package com.github.oreganoli.starchart;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
+import io.javalin.plugin.json.JavalinJson;
 
 public class Main {
     private static StarRepository repo;
@@ -16,6 +19,9 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
+        Gson gson = new GsonBuilder().create();
+        JavalinJson.setFromJsonMapper(gson::fromJson);
+        JavalinJson.setToJsonMapper(gson::toJson);
         Javalin app = Javalin.create(config -> {
             config.addStaticFiles("static", Location.EXTERNAL);
         }).start(9000);
