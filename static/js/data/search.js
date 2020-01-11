17e1849412fe97dc104e450e5677da400909b7f1
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-11 16:52:25 +0100

    wrote a search criterion default struct

---
+++ static/js/data/search.js
@@ -0,0 +1,8 @@
+export const defaultSearch = () => ({
+    constellation: null,
+    distance_parsecs: null,
+    temperature: null,
+    apparent_magnitude: null,
+    hemisphere: null,
+    potential_supernovae: null,
+});
