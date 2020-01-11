5ad7a692c4483a514520cfd5e716bb95ad233fed
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-11 14:17:44 +0100

    corrected normalize.css URL and created a main root element

--- static/js/index.js
+++ static/js/index.js
@@ -3,5 +3,6 @@ import htm from "/js/web_modules/htm.js";
 
 const appName = "starchart";
 const html = htm.bind(h);
+const root = document.getElementById("root");
 const Hello = () => html`<h1>${appName}</h1>`;
-render(html`${Hello()}`, document.body);
+render(html`${Hello()}`, root);
