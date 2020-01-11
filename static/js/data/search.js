ee2c4e07cd80278b852a70cc3a22105fb8db8536
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-11 17:24:43 +0100

    wrote a helper function for creating ranges

--- static/js/data/search.js
+++ static/js/data/search.js
@@ -6,6 +6,7 @@ export const defaultSearch = () => ({
     hemisphere: null,
     potential_supernovae: null,
 });
+export const range = (min, max) => ({min, max});
 
 export const setSearch = (criteria) => ({type: "SET_SEARCH", data: criteria});
 export const clear = () => ({type: "CLEAR_SEARCH"});
