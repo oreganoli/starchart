9151c8692513a5223d8929e331fd181c41ebfcb0
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-01-28 00:51:05 +0100

    filled out the search window

--- static/js/components/SearchModal.js
+++ static/js/components/SearchModal.js
@@ -16,6 +16,7 @@ const accept = (criteria, checks) => {
     if (!checks.constellation) {
         crit.constellation = null;
     }
+    console.log(crit);
     AppStore.update(s => {
         s.searchWindow = false;
         s.search = crit;
@@ -41,7 +42,44 @@ export const SearchModal = () => {
         <div className="input_row">
             <input type={"text"} disabled={!checks.constellation} value={tempCrit.constellation} onChange={(e) => setTempCrit({...tempCrit, constellation: e.target.value})}/>
         </div>
-        <div></div>
+        <input type="checkbox"/>
+        <label>Distance <b>[pc]</b></label>
+        <div className="input_row">
+            <input style={"width: 6ch;"} type={"number"} min={0} step={0.01}/><b>[pc]</b>
+             - 
+            <input style={"width: 6ch;"} type={"number"} min={0} step={0.01}/><b>[pc]</b>
+        </div>
+        <input type="checkbox"/>
+        <label>Temperature</label>
+        <div className="input_row">
+        <input style={"width: 6ch;"} type={"number"} min={4000} step={0.01}/><b>[°C]</b>
+        -
+        <input style={"width: 6ch;"} type={"number"} min={4000} step={0.01}/><b>[°C]</b>
+        </div>
+        <input type="checkbox"/>
+        <label>Apparent magnitude</label>
+        <div className="input_row">
+        <input style={"width: 6ch;"} type={"number"} min={-26.74} max={15.00} step={0.01}/>
+        -
+        <input style={"width: 6ch;"} type={"number"} min={-26.74} max={15.00} step={0.01}/>
+        </div>
+        <label>Hemisphere</label>
+        <div className="input_row">
+            <select>
+                <option value={null} selected>Any</option>
+                <option value="Northern">Northern</option>
+                <option value="Southern">Southern</option>
+                <option value="Equatorial">Equatorial</option>
+            </select>
+        </div>
+        <label>Potential supernovae</label>
+        <div className="input_row">
+            <select>
+                <option value={null} selected>Either</option>
+                <option value={true}>Yes</option>
+                <option value={false}>No</option>
+            </select>
+        </div>
         <div className="button_row">
             <button onClick={() => accept(tempCrit, checks)}>🔍 Search</button>
             <button onClick={() => reject()}>Cancel</button>
